31b2e36553ca58f116ea82cd8e3912e3d6706c82
molalenim <molaleni.mabaso@gmail.com>
2024-01-18 18:00:52 -0500

    b) Class 3 Exercises: Strings & Arrays - 5&6

--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -5,6 +5,19 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
 
+let combinedArrays = holdCabinet1.concat(holdCabinet2); 
+console.log(combinedArrays);
+console.log(holdCabinet1);
+
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
 
+console.log(holdCabinet1.slice(0,2));
+console.log(holdCabinet2.slice(0,2));
+
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+
+holdCabinet1.reverse();
+console.log(holdCabinet1);
+
+holdCabinet2.sort();
+console.log(holdCabinet2);
