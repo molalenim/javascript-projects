e66d92895280cdc8033ebe5e6af830f4eec30345
molalenim <molaleni.mabaso@gmail.com>
2024-01-18 22:20:01 -0500

    c) Class 3 Studio: Strings & Arrays

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -5,42 +5,45 @@ let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
+
+
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	let check;
-	let output;
+	let check = protoArray1.split(',')
+	console.log(check);
+	let output = '';
 	//TODO: 2. write the code required for this step
-
+output = check.reverse().join();
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
 
 //3)
 function semiDash() {
-	let check;
-	let output;
+	let check = protoArray2.split(';');
+	let output = '';
 //TODO: write the code required for this step
-
+output = check.sort().join('-');
   
 	return output;
 }
 
 //4)
 function reverseSpaces() {
-	let check;
-	let output;
+	let check = protoArray3.split(' ');
+	let output = '';
   //TODO: write the code required for this step
-
+output = check.sort().reverse().join(' ');
 	return output;
 }
 
 //5)
 function commaSpace() {
-	let check;
-	let output;
+	let check = protoArray4.split(', ');
+	let output = '';
 	//TODO: write the code required for this step
-  
+  output = check.reverse().join(','); 
 	return output;
 }
 
