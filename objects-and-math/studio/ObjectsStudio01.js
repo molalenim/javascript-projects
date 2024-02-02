6e14da9ff65e5eb7100da2d956868ac0dc3c3048
molalenim <molaleni.mabaso@gmail.com>
2024-02-01 22:59:39 -0500

    c) Class 7 Studio: Objects and Math

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,55 +1,101 @@
 // Code your selectRandomEntry function here:
 
-
+function selectRandomEntry(arr) {
+  // change to return 3 random numbers from the array
+  let randomArray = [];
+  let i = Math.floor(Math.random() * arr.length);
+  randomArray.push(arr[i]);
+  return randomArray;
+}
 // Code your buildCrewArray function here:
+function buildAcrew(idArray, animalArray) {
+  let selectedCrew = [];
+  for (i = 0; i < animalArray.length; i++) {
+    if (idArray.includes(animalArray[i].astronautID)) {
+      selectedCrew.push(animalArray[i].name);
+    }
+    
+  }
+  return selectedCrew;
+}
 
 
 let idNumbers = [291, 414, 503, 599, 796, 890];
 
 // Here are the candidates and the 'animals' array:
 let candidateA = {
-  'name':'Gordon Shumway',
-  'species':'alf',
-  'mass':90,
-  'o2Used':function(hrs){return 0.035*hrs},
-  'astronautID':414
+  name: "Gordon Shumway",
+  species: "alf",
+  mass: 90,
+  o2Used: function (hrs) {
+    return 0.035 * hrs;
+  },
+  astronautID: 414,
 };
 let candidateB = {
-  'name':'Lassie',
-  'species':'dog',
-  'mass':19.1,
-  'o2Used':function(hrs){return 0.030*hrs},
-  'astronautID':503
+  name: "Lassie",
+  species: "dog",
+  mass: 19.1,
+  o2Used: function (hrs) {
+    return 0.03 * hrs;
+  },
+  astronautID: 503,
 };
 let candidateC = {
-  'name':'Jonsey',
-  'species':'cat',
-  'mass':3.6,
-  'o2Used':function(hrs){return 0.022*hrs},
-  'astronautID':796
+  name: "Jonsey",
+  species: "cat",
+  mass: 3.6,
+  o2Used: function (hrs) {
+    return 0.022 * hrs;
+  },
+  astronautID: 796,
 };
 let candidateD = {
-  'name':'Paddington',
-  'species':'bear',
-  'mass':31.8,
-  'o2Used':function(hrs){return 0.047*hrs},
-  'astronautID':291
+  name: "Paddington",
+  species: "bear",
+  mass: 31.8,
+  o2Used: function (hrs) {
+    return 0.047 * hrs;
+  },
+  astronautID: 291,
 };
 let candidateE = {
-  'name':'Pete',
-  'species':'tortoise',
-  'mass':417,
-  'o2Used':function(hrs){return 0.010*hrs},
-  'astronautID':599
+  name: "Pete",
+  species: "tortoise",
+  mass: 417,
+  o2Used: function (hrs) {
+    return 0.01 * hrs;
+  },
+  astronautID: 599,
 };
 let candidateF = {
-  'name':'Hugs',
-  'species':'ball python',
-  'mass':2.3,
-  'o2Used':function(hrs){return 0.018*hrs},
-  'astronautID':890
+  name: "Hugs",
+  species: "ball python",
+  mass: 2.3,
+  o2Used: function (hrs) {
+    return 0.018 * hrs;
+  },
+  astronautID: 890,
 };
 
-let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
+let animals = [
+  candidateA,
+  candidateB,
+  candidateC,
+  candidateD,
+  candidateE,
+  candidateF,
+];
 
 // Code your template literal and console.log statements:
+// console.log(selectedCrew);
+// todo call selectrandomentry to create array of 3 random IDs
+let selectedCrewIDs = selectRandomEntry(idNumbers);
+let announcementArray = buildAcrew(selectedCrewIDs, animals);
+console.log(announcementArray[0]);
+
+// console.log(
+//   `${announcementArray[0]}, ${announcementArray[1]}, ${announcementArray[2]}, are going`
+// );
+
+
--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,7 +1,29 @@
 // Code your orbitCircumference function here:
 
+function calculateCircumference(radius) {
+  let Circumference = 2 * Math.PI * radius;
+  return Math.round(Circumference);
+}
+console.log(calculateCircumference(3));
+console.log(calculateCircumference(100));
 
 // Code your missionDuration function here:
+
+//Define the missionDuration function to take three parameters - the number of orbits completed, the orbit radius, and the orbital speed. Set the default radius to 2000 km and the default orbital speed to 28000 km/hr.≈
+//Calculate how long it will take our animals to complete a certain number of orbits (time = distance/speed). Round the answer to 2 decimal places, then return the result. For example, with the default radius and speed, 5 orbits will take about 2.24 hours.
+
+
+
+
+function missionDuration (orbitsCompleted, orbitRadius = 2000, orbitalSpeeed = 2800){
+  let time = (2 * Math.PI * orbitRadius * orbitsCompleted) /orbitalSpeeed;
+  time = time.toFixed(2);
+  return time;
+}
+
+let orbits = 5;
+let result = missionDuration(orbits);
+console.log(`The mission duration for ${orbits} orbits is ${result} hours.`);
 
 
 // Copy/paste your selectRandomEntry function here:
