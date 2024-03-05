226f2408f2a5335c349b0162707e98b4cabd2b12
molalenim <molaleni.mabaso@gmail.com>
2024-03-05 08:03:42 -0500

    c) Class 16 Studio: Fetch and JSON - fetch with modern async/await syntax

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,25 +1,64 @@
 //TODO: Add Your Code Below
-window.addEventListener('load', function(){
-    
-    const container = document.getElementById('container');
+window.addEventListener("load", function () {
+  const container = document.getElementById("container");
 
-    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response){
-        response.json().then(function(data){
-            console.log(data);
-            
-            //bonus mission 1
-            data.sort(function(a, b) {
-                return a.hoursInSpace < b.hoursInSpace ? 1 : -1; 
-            });
-            //bonus mission 3
-            const count = document.getElementById("count");
-            count.innerHTML =`The total number of astronauts is ${data.length}`; 
-            for (let i=0; i < data.length; i++){
-                let astronaut = data[i];
-                //bonus mission2 
-                let activeClass = astronaut.active ? "active": "";
+    //   fetch(
+    //     "https://handlers.education.launchcode.org/static/astronauts.json"
+    //   ).then(function (response) {
+    //     response.json().then(function (data) {
+    //       console.log(data);
 
-                container.innerHTML += `
+    //       //bonus mission 1
+    //       data.sort(function (a, b) {
+    //         return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+    //       });
+    //       //bonus mission 3
+    //       const count = document.getElementById("count");
+    //       count.innerHTML = `The total number of astronauts is ${data.length}`;
+    //       for (let i = 0; i < data.length; i++) {
+    //         let astronaut = data[i];
+    //         //bonus mission2
+    //         let activeClass = astronaut.active ? "active" : "";
+
+    //         container.innerHTML += `
+    //                 <div class="astronaut">
+    //                     <div class="bio">
+    //                         <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+    //                         <ul>
+    //                         <li>Hours in space: ${astronaut.hoursInSpace}</li>
+    //                         <li class="${activeClass}">Active: ${
+    //           astronaut.active
+    //         }</li>
+    //                         <li>Skills: ${astronaut.skills.join(", ")}</li>
+    //                         </ul>
+    //                     </div>
+    //                 <img class="avatar" src="${astronaut.picture}">
+    //                 </div>
+    //                 `;
+    //       }
+    //     });
+    //   });
+
+  //fetch with modern async/await syntax
+
+  async function fetchAndDisplayAstronauts() {
+        let response = await fetch(
+        "https://handlers.education.launchcode.org/static/astronauts.json");
+        let data = await response.json();
+
+        //bonus mission 1
+        data.sort(function (a, b) {
+        return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+        });
+        //bonus mission 3
+        const count = document.getElementById("count");
+        count.innerHTML = `The total number of astronauts is ${data.length}`;
+        for (let i = 0; i < data.length; i++) {
+        let astronaut = data[i];
+        //bonus mission2
+        let activeClass = astronaut.active ? "active" : "";
+
+        container.innerHTML += `
                 <div class="astronaut">
                     <div class="bio">
                         <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
@@ -32,14 +71,8 @@ window.addEventListener('load', function(){
                 <img class="avatar" src="${astronaut.picture}">
                 </div>
                 `;
-            
-            }    
-        });
-
-
-
-    });
-
-
-
+        }
+    }
+    //call async function
+    fetchAndDisplayAstronauts();
 });
